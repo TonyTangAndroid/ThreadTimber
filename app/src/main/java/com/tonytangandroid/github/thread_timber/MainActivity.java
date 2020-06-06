2b19c5249bbeb430fb91dbb4224dac3a85dac849
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-05 20:13:30 -0700

    Initial commit

---
+++ app/src/main/java/com/tonytangandroid/github/thread_timber/MainActivity.java
@@ -0,0 +1,23 @@
+package com.tonytangandroid.github.thread_timber;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import timber.log.ThreadTree;
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Timber.plant(new ThreadTree());
+    setContentView(R.layout.activity_main);
+    Timber.i("Log message in Main Thread");
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        Timber.v("Log message in Worker Thread");
+      }
+    }).start();
+  }
+}
